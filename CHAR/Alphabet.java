de1d0e3b3fd0c4f5a9af94fa79661bcacc7f2c59
Pyramid302614 <151288894+Pyramid302614@users.noreply.github.com>
2025-03-07 15:26:33 -0800

    Add files via upload

---
+++ CHAR/Alphabet.java
@@ -0,0 +1,39 @@
+package CHAR.Functions;
+
+public class Alphabet {
+
+    public char[] getCharAlphabet() {
+
+        return new char[] {
+                'a',
+                'b',
+                'c',
+                'd',
+                'e',
+                'f',
+                'g',
+                'h',
+                'i',
+                'f',
+                'g',
+                'h',
+                'i',
+                'j',
+                'k',
+                'l',
+                'm',
+                'o',
+                'p',
+                'q',
+                'r',
+                's',
+                't',
+                'v',
+                'w',
+                'x',
+                'y',
+                'z'
+        };
+    }
+
+}
